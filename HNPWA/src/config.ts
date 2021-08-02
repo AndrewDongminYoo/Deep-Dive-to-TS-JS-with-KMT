6130973209f6b491a5e0316370e9aab5d00b46cc
AndrewDongminYoo <ydm2790@gmail.com>
2021-08-02 14:42:59 +0900

    importing

---
+++ HNPWA/src/config.ts
@@ -0,0 +1,14 @@
+export const NEWS_URL: string = 'https://api.hnpwa.com/v0/news/1.json';
+export const CONTENT_URL: string = 'https://api.hnpwa.com/v0/item/@id.json';
+
+
+// function applyApiMixins(targetClass: any, baseClasses: any[]): void {
+//   baseClasses.forEach((baseClass) => {
+//     Object.getOwnPropertyNames(baseClass.prototype).forEach((name) => {
+//       const descriptor = Object.getOwnPropertyDescriptor(baseClass.prototype, name);
+//       if (descriptor) {
+//         Object.defineProperty(targetClass.prototype, name, descriptor)
+//       };
+//     });
+//   });
+// };
